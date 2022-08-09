76478ec54e56a4ea83b7f36562f06b12bda128f4
qaws1270 <halamg851@gmail.com>
2022-08-10 01:56:01 +0900

    what

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT;
+const result = "";
 
 app.get("/", function (req, res) {
-  if (!req.query.email) {
+  if (!req.query.res) {
     res.send("No query");
     return;
   }
-  const { email, pw, uuid, result } = req.query;
-  console.log(email, pw, uuid, result);
+  result += req.query.res + "<br>";
   res.send("OK");
 });
-
-app.listen(80, function () {
-  console.log("Example app listening on port 80!");
+app.get("/see", function (req, res) {
+  if (res.query.id == "mofucker") {
+    res.send(result);
+  } else {
+    res.send("Nugumma");
+  }
 });
+
+app.listen(port, function () {});
