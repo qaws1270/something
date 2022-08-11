617dc5c82a44ac59ce4d56066bb458ad9cbe6438
qaws1270 <halamg851@gmail.com>
2022-08-11 11:23:50 +0900

    dd

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
+const cors = require("cors");
 const result = "";
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", function (req, res) {
   if (!req.query.res) {
